a6e90400c1fcd5d803930058d6da0dd0e3a3513e
paolo-de-marinis <paolodemarinis1993@gmail.com>
2026-09-17 17:22:32 +0200

    Implement Karatsuba multiplication for 128-bit integers and add new test file

--- src/poly.rs
+++ src/poly.rs
@@ -17,3 +17,21 @@ pub fn clmul(a: u64, b: u64) -> u128 {
     }
     result
 }
+
+pub fn clmul128(a: u128, b: u128) -> (u128,u128) {
+    let mut a0 = a as u64;
+    let mut a1 = (a >> 64) as u64;
+    let mut b0 = b as u64;
+    let mut b1 = (b >> 64) as u64;
+    let mut lo: u128 = 0;
+    let mut hi: u128 = 0;
+    // Karatsuba products
+    let mut p0 = clmul(a0, b0);
+    let mut p2 = clmul(a1, b1);
+    // let mut p1 = clmul(a0,b1)^clmul(a1,b0);
+    let p1 = clmul(a0 ^ a1, b0 ^ b1) ^ p0 ^ p2;
+    
+    lo = p0 ^ (p1 << 64);
+    hi = p2 ^ (p1 >> 64);
+    (lo,hi)
+}
